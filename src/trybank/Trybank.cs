51dd488550802cfd4767cc99b3c1587c1538aec6
MartinGBB <martingbrazon@gmail.com>
2024-07-20 17:14:27 -0300

    feat: Requisito 6 - retira saldo

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -83,7 +83,13 @@ public class TrybankLib
     // 6. Construa a funcionalidade de sacar dinheiro
     public void Withdraw(int value)
     {
-        throw new NotImplementedException();
+        validateUserLogget();
+        int cash = Bank[loggedUser, 3];
+
+        if (cash < value)
+            throw new InvalidOperationException("Saldo insuficiente");
+
+        Bank[loggedUser, 3] -= value;
     }
 
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
